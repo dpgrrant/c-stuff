e26735e9fe1e5aee97941ac6730cf484862572e9
dpgrrant <80363299+dpgrrant@users.noreply.github.com>
2022-01-14 10:10:46 -0500

    main

--- main.c
+++ main.c
@@ -4,12 +4,30 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/types.h> 
 
 int main(){
 
+int pid = fork();
+char *x[2];
+x[0]="ls";
+x[1]=NULL;
+
+
+if(pid == 0)
+{
+printf("I am a child process\n");
+execv(x[0],x);
+}
+else
+{
+printf("I am the parent process\n");
+waitpid(pid,NULL,0);
+}
+printf("We did it! We did an ls!\n");
+return 0;
 
 
 
-    
+
 }
