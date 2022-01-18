4b2d227916d662d0037691d2b2a9099e6aa5f993
dpgrrant <80363299+dpgrrant@users.noreply.github.com>
2022-01-18 14:17:10 -0500

    parser and echo USER

--- main.c
+++ main.c
@@ -6,28 +6,117 @@
 #include <string.h>
 #include <sys/types.h> 
 
+
+
+typedef struct {
+int size;
+char **items;
+} tokenlist;
+
+char *get_input(void);
+tokenlist *get_tokens(char *input);
+tokenlist *new_tokenlist(void);
+void add_token(tokenlist *tokens, char *item);
+void free_tokens(tokenlist *tokens);
+
+
 int main(){
+char *user = getenv("USER");
+char *pwd = getenv("PWD");
+char *machine = getenv("MACHINE");
+char *path = getenv("PATH");
+char *home = getenv("HOME");
 
-int pid = fork();
-char *x[2];
-x[0]="ls";
-x[1]=NULL;
 
 
-if(pid == 0)
-{
-printf("I am a child process\n");
-execv(x[0],x);
+
+while (1) {
+    printf("\n%d@%d : >",user,machine,pwd);
+    /* input contains the whole command
+    * tokens contains substrings from input split by spaces
+    */
+    char *input = get_input();
+    tokenlist *tokens = get_tokens(input);
+
+    if(strcmp(tokens->items[0],"echo")==0){
+        char *value=getenv(tokens->items[1]);
+        printf("%s\n",value);
+    }
+
+    for (int i = 0; i < tokens->size; i++) {
+        printf("token %d: (%s)\n", i, tokens->items[i]);
+    }
+
+
+
+
+
+
+
+
+
+    free(input);
+    free_tokens(tokens);
 }
-else
-{
-printf("I am the parent process\n");
-waitpid(pid,NULL,0);
-}
-printf("We did it! We did an ls!\n");
 return 0;
+}
 
-
-
-
+tokenlist *new_tokenlist(void)
+{
+tokenlist *tokens = (tokenlist *) malloc(sizeof(tokenlist));
+tokens->size = 0;
+tokens->items = (char **) malloc(sizeof(char *));
+tokens->items[0] = NULL; /* make NULL terminated */
+return tokens;
+}
+void add_token(tokenlist *tokens, char *item)
+{
+int i = tokens->size;
+tokens->items = (char **) realloc(tokens->items, (i + 2) * sizeof(char *));
+tokens->items[i] = (char *) malloc(strlen(item) + 1);
+tokens->items[i + 1] = NULL;
+strcpy(tokens->items[i], item);
+tokens->size += 1;
+}
+char *get_input(void)
+{
+char *buffer = NULL;
+int bufsize = 0;
+char line[5];
+while (fgets(line, 5, stdin) != NULL) {
+int addby = 0;
+char *newln = strchr(line, '\n');
+if (newln != NULL)
+addby = newln - line;
+else
+addby = 5 - 1;
+buffer = (char *) realloc(buffer, bufsize + addby);
+memcpy(&buffer[bufsize], line, addby);
+bufsize += addby;
+if (newln != NULL)
+break;
+}
+buffer = (char *) realloc(buffer, bufsize + 1);
+buffer[bufsize] = 0;
+return buffer;
+}
+tokenlist *get_tokens(char *input)
+{
+char *buf = (char *) malloc(strlen(input) + 1);
+strcpy(buf, input);
+tokenlist *tokens = new_tokenlist();
+char *tok = strtok(buf, " ");
+while (tok != NULL) {
+add_token(tokens, tok);
+tok = strtok(NULL, " ");
+}
+free(buf);
+return tokens;
+}
+void free_tokens(tokenlist *tokens)
+{
+for (int i = 0; i < tokens->size; i++)
+free(tokens->items[i]);
+free(tokens->items);
+free(tokens);
 }
